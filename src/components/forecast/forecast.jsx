bb08b13be5c83df33a238c48157a59d6d6106f4c
yoswer07 <yoswer07@gmail.com>
2023-05-24 12:46:43 -0400

    Cambio de la interfaz por precision del api

--- src/components/forecast/forecast.jsx
+++ src/components/forecast/forecast.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemPanel, AccordionItemHeading } from 'react-accessible-accordion'
 import '../forecast/forecast.css'
 
-const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 const Forecast = ({ data }) => {
-  const dayInWeek = new Date().getDay()
-  const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInWeek))
+//   const dayInWeek = new Date().getDay()
+//   const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInWeek))
   return (
     <>
-        <label className= 'title'>Daily</label>
+        <label className= 'title'>Every 3 hours</label>
         <Accordion allowZeroExpanded>
             {data.list.slice(0, 7).map((item, idx) => (
                 <AccordionItem key={idx}>
@@ -18,7 +18,7 @@ const Forecast = ({ data }) => {
                         <AccordionItemButton>
                             <div className="daily-item">
                                 <img src={`icons/${item.weather[0].icon}.png`} alt="weather" className="icon-small" />
-                                <label className="day">{forecastDays[idx]}</label>
+                                <label className="day">{item.dt_txt}</label>
                                 <label className="description">{item.weather[0].description}</label>
                                 <label className="min-max">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
                             </div>
